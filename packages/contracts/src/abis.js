7aa2984f443b980e1cf8e65f8f853cbd4c47ff0d
Yueming Xu <yxucolo@gmail.com>
2021-05-25 15:30:39 -0600

    add contracts

---
+++ packages/contracts/src/abis.js
@@ -0,0 +1,11 @@
+import erc20Abi from "./abis/erc20.json";
+import ownableAbi from "./abis/ownable.json";
+import enftAbi from "./abis/enft.json";
+
+const abis = {
+  erc20: erc20Abi,
+  ownable: ownableAbi,
+  enft: enftAbi,
+};
+
+export default abis;
---
+++ packages/contracts/src/addresses.js
@@ -0,0 +1,10 @@
+// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+
+// add testing ENFT contract deployed on Rinkeby
+const addresses = {
+  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  rinkenft: "0xc98e641bBbFab95BD118dA10C4D26Cdc3F1bd387",
+};
+
+export default addresses;
